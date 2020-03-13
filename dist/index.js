202c255bfe24454f6e5dfedc2cf4fe178ee3145b
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2020-03-13 23:42:49 +0100

    remove sudo

--- dist/index.js
+++ dist/index.js
@@ -991,7 +991,7 @@ function run() {
     });
 }
 const installYamllint = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield exec.exec(`sudo pip3 install yamllint`);
+    yield exec.exec(`pip3 install yamllint`);
 });
 const runYamllint = (yamlCommand) => __awaiter(void 0, void 0, void 0, function* () {
     yield exec.exec(`yamllint ${yamlCommand}`);
